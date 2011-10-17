70621cef39696b703207bdbd4f3a2a6f7650618b
Sami Kukkonen <vixenvineyard@gmail.com>
2011-10-17 22:53:05 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+var xml = require('o3-xml'),
+    request = require('request')
+
+var Client = exports.Client = function Client(appKey) {
+  this.appKey = appKey
+}
+
+Client.prototype.query = function(input, cb) {
+  var uri = 'http://api.wolframalpha.com/v2/query?input=' + encodeURIComponent(input) + '&primary=true&appid=' + this.appKey
+  
+  request(uri, function(error, response, body) {
+    if(!error && response.statusCode == 200) {
+      var doc = xml.parseFromString(body),
+          rootNode = doc.documentElement
+      
+      if(rootNode.getAttribute('error') != 'false') {
+        var message = rootNode.selectSingleNode('descendant-or-self::error/msg').nodeValue
+        return cb(message, null)
+      } else {
+        var result = [],
+            podNodes = rootNode.selectNodes('descendant-or-self::pod')
+          
+        for(var i = 0; i < podNodes.length; i++) {
+          var podNode = podNodes[i],
+              pod = { subpods: [] }
+          
+          pod.title = podNode.getAttribute('title')
+          if(podNode.getAttribute('primary') == 'true')
+            pod.primary = true
+          
+          var subpodNodes = podNode.selectNodes('descendant-or-self::subpod')
+          for(var j = 0; j < subpodNodes.length; j++) {
+            var subpodNode = subpodNodes[j]
+              , subpod = {}
+            
+            subpod.title = subpodNode.getAttribute('title')
+            subpod.value = subpodNode.selectSingleNode('descendant-or-self::plaintext').nodeValue
+            
+            pod.subpods.push(subpod)
+          }
+          
+          result.push(pod)
+        }
+        
+        return cb(null, result)
+      }
+    }
+  })
+}
+
+exports.createClient = function(appKey) {
+  return new Client(appKey)
+}
