76630a80be280536ff164fe5eacd4b3cb48458dd
Sami Kukkonen <vixenvineyard@gmail.com>
2011-10-28 18:06:42 +0300

    Add tests

---
+++ test/valid_response.js
@@ -0,0 +1,14 @@
+var test = require('tap').test,
+    wolfram = require('../')
+
+test('valid response', function(t) {
+  var client = wolfram.createClient(process.env.WOLFRAM_APPID)
+  
+  client.query("integrate 2x", function(err, result) {
+    t.notOk(err, "err should be null")
+    t.equal(result.length, 2, "result length should be 2")
+    t.equal(result[0].primary, true, "first result should be primary")
+    t.equal(result[0].subpods[0].value, ' integral 2 x dx = x^2+constant', 'result should be constant')
+    t.end()
+  })
+})
