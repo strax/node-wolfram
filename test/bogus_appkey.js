37e4b20e39d5f630a116bf33482d0e7165347bf0
Sami Kukkonen <vixenvineyard@gmail.com>
2011-10-28 23:35:27 +0300

    Test invalid app ID scenario

---
+++ test/bogus_appkey.js
@@ -0,0 +1,11 @@
+var test = require('tap').test,
+    wolfram = require('../')
+
+test('bogus application id', function(t) {
+  var client = wolfram.createClient("bogus-trololoo")
+  
+  client.query("integrate 2x", function(err, result) {
+    t.type(err, "string", "error should be a string describing the message")
+    t.end()
+  })
+})
