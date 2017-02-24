9b0fd397fa913b31834f0f6edbbbadaaa48866e9
Sami Kukkonen <sami@samikukkonen.fi>
2017-02-24 18:15:28 +0200

    Fix test

--- test/valid_response.js
+++ test/valid_response.js
@@ -3,7 +3,7 @@ var test = require('tap').test,
 
 test('valid response', function(t) {
   var client = wolfram.createClient(process.env.WOLFRAM_APPID)
-  
+
   t.test("integrate 2x", function(t) {
     client.query("integrate 2x", function(err, result) {
       t.notOk(err, "err should be null")
@@ -11,7 +11,7 @@ test('valid response', function(t) {
       t.ok(result.length)
       t.equal(result.length, 2, "result length should be 2")
       t.equal(result[0].primary, true, "first pod should be primary")
-      t.equal(result[0].subpods[0].value, ' integral 2 x dx = x^2+constant', 'result should be constant')
+      t.equal(result[0].subpods[0].value, ' integral 2 x dx = x^2 + constant', 'result should be constant')
       t.ok(result[0].subpods[0].image, "result should have an image representation")
       t.ok(result[0].title, "result should have a title of Indefinite integral")
       t.end()
