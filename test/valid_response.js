fb3b6b221c43a0f3d6bbabee7970f4710312bd69
Sami Kukkonen <sami@samikukkonen.fi>
2018-10-21 23:07:36 +0300

    Update test assertion to match expected output

--- test/valid_response.js
+++ test/valid_response.js
@@ -11,7 +11,7 @@ test('valid response', function(t) {
       t.ok(result.length)
       t.equal(result.length, 2, "result length should be 2")
       t.equal(result[0].primary, true, "first pod should be primary")
-      t.equal(result[0].subpods[0].value, ' integral 2 x dx = x^2 + constant', 'result should be constant')
+      t.equal(result[0].subpods[0].value, 'integral2 x dx = x^2 + constant', 'result should be constant')
       t.ok(result[0].subpods[0].image, "result should have an image representation")
       t.ok(result[0].title, "result should have a title of Indefinite integral")
       t.end()
